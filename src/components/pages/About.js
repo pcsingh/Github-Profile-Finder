68dde96a8414170cb8ed14307fbf7b956544bf4d
pcsingh <premchandra.singh.5268@gmail.com>
2019-08-23 19:32:41 +0530

    About Us Updated

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ const About = () => {
 	return (
 		<Fragment>
 			<h1>About Page</h1>
-			<p>It's an Github Finder.</p>
+			<p>It's an GitHub profile finder web app, where you can see your friend repositories with simple and clean view just by searching his/her username.</p>
 			<p><b>Version: </b>1.0.0</p>
 		</Fragment>	
 	)
