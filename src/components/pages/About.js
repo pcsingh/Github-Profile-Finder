9a11702286e48fb028b2488c9b61979243aeaf9d
pcsingh <premchandra.singh.5268@gmail.com>
2019-08-31 12:38:10 +0530

    About Edited

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,6 +5,7 @@ const About = () => {
 		<Fragment>
 			<h1>About Page</h1>
 			<p>It's an GitHub profile finder web app, where you can see your friend repositories with simple and clean view just by searching his/her username.</p>
+			<p><b>Author: </b>Prem</p>
 			<p><b>Version: </b>1.0.0</p>
 		</Fragment>	
 	)
